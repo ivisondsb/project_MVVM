8195af54a3b6108e11cc6ef7e37167165c044550
Ivison <Ivisonk0@gmail.com>
2022-01-20 11:55:12 -0300

    create viewmodelfactory

---
+++ app/src/main/java/com/ivisondsb/projectmvvm/viewmodel/main/MainViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.ivisondsb.projectmvvm.viewmodel.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ivisondsb.projectmvvm.repositories.MainRepository
+import java.lang.IllegalArgumentException
+
+class MainViewModelFactory constructor(private val repository: MainRepository) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            MainViewModel(this.repository) as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+
+}
